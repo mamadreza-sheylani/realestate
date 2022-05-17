30573250e0957a030b9ab8da4b1d9fe90516518a
mamad <mamadrezasheylani@gmail.com>
2022-05-17 13:07:18 +0430

    (feat): added porgress bar

--- components/imageScrollbar.js
+++ components/imageScrollbar.js
@@ -3,32 +3,35 @@ import Image from "next/image";
 import { Box, Icon, Flex } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
   return (
     <Flex justifyContent="center" alignItems="center" marginRight="8">
       <Icon
         as={FaArrowAltCircleLeft}
-        onClick={scrollPrev}
+        onClick={() => scrollPrev()}
         fontSize="2xl"
         cursor="pointer"
       />
     </Flex>
   );
 };
+
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
   return (
     <Flex justifyContent="center" alignItems="center" marginRight="8">
       <Icon
         as={FaArrowAltCircleRight}
-        onClick={scrollNext}
+        onClick={() => scrollNext()}
         fontSize="2xl"
         cursor="pointer"
       />
     </Flex>
   );
 };
+
 const ImageScrollbar = ({ data }) => {
   return (
     <ScrollMenu
@@ -46,20 +49,13 @@ const ImageScrollbar = ({ data }) => {
         >
           <Image
             src={image.url}
-            alt="properties"
-            width="1000px"
-            height="500px"
-            unoptimized
-          />
-          {/* <Image
-            src={image.url}
             placeholder="blur"
             blurDataURL={image.url}
             alt="properties"
             width="1000px"
             height="500px"
             sizes="(max-width:500px) 100px,(max-width:1024px) 400px,1000px"
-          /> */}
+          />
         </Box>
       ))}
     </ScrollMenu>
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -25,8 +25,94 @@ const PropertyDetails = ({
   },
 }) => {
   return (
-    <Box maxW="100px" margin="auto" p="4">
+    <Box
+      minH={400}
+      margin="auto"
+      p="4"
+      border="1px"
+      borderColor="gray.200"
+      borderRadius={8}
+      marginTop="3"
+      marginBottom={3}
+    >
       {photos && <ImageScrollbar data={photos} />}
+      <Box w="full" marginTop={4} p="6">
+        <Flex paddingTop={2} alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight={3} color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="large">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url} />
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          padding="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+          <BsGridFill />
+        </Flex>
+        <Box marginTop={2}>
+          <Text fontSize="lg" fontWeight="medium" marginTop={4}>
+            {title}
+          </Text>
+          <Text fontSize="lg" color="gray.600" marginTop={4}>
+            {description}
+          </Text>
+        </Box>
+        <Flex
+          flexWrap="wrap"
+          textTransform="uppercase"
+          justifyContent="space-between"
+          marginTop={5}
+          border="1px"
+          borderColor="gray.200"
+          p="4"
+          borderRadius="8px"
+        >
+          <Flex
+            justifyContent="space-between"
+            w="400px"
+            borderBottom="1"
+            borderColor="gray.100"
+            marginBottom={5}
+          >
+            <Text>Type</Text>
+            <Text fontWeight="bold">{type}</Text>
+          </Flex>
+          <Flex
+            justifyContent="space-between"
+            w="400px"
+            borderBottom="1"
+            borderColor="gray.100"
+            marginBottom={5}
+          >
+            <Text>Purpose</Text>
+            <Text fontWeight="bold">{purpose}</Text>
+          </Flex>
+          {furnishingStatus && (
+            <Flex
+              justifyContent="space-between"
+              w="400px"
+              borderBottom="1"
+              borderColor="gray.100"
+              marginBottom={5}
+            >
+              <Text>Furnishing Status</Text>
+              <Text fontWeight="bold">{furnishingStatus}</Text>
+            </Flex>
+          )}
+        </Flex>
+      </Box>
     </Box>
   );
 };
